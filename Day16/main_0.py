297439600f70cddeec1b71dc1d50c898e41009dc
Manuel-Tellechea <tellechea19@gmail.com>
2021-12-27 22:34:46 +0100

    completed day 16

---
+++ Day16/main_0.py
@@ -0,0 +1,23 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("cyan")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+
+print(table.align)
+print(table)
